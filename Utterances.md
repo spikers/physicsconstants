9b1810004365c9e176157ea27c44196cc013e506
spikers <timfengineering@gmail.com>
2017-06-09 09:56:41 -0700

    Completed. Just need to minimize.

---
+++ Utterances.md
@@ -0,0 +1,109 @@
+GetConstant Tell me Avogadro's Number
+GetConstant Give me Avogadro's Number
+GetConstant What is Avogadro's Number
+
+GetConstant Tell me {Constant}
+GetConstant Give me {Constant}
+GetConstant What is {Constant}
+GetConstant What is the {Constant}
+GetConstant How much is {Constant}
+GetConstant How much is a {Constant}
+GetConstant How much is an {Constant}
+GetConstant How big is {Constant}
+GetConstant How big is a {Constant}
+GetConstant How big is an {Constant}
+GetConsatnt How long is {Constant}
+GetConsatnt How long is a {Constant}
+GetConsatnt How long is an {Constant}
+
+GetConstant What is the constant of {Constant}
+
+Samples:
+What is the Boltzmann Constant
+What is Avogadro's Constant
+How big is Planck Mass
+
+
+Slots:
+Constant
+    Avogadro's Number | Speed of Light | The Speed of Light
+
+Ask Physics Constants what is The Speed of Light
+
+Constants: See constants.js
+
+
+Newtonian Constant of Gravitation
+AKA
+Newtonian Constant of Gravitation
+Newtonian Constant of Gravity
+Newtonian Gravitation Constant
+Newtonian Gravity Constant
+Newton's Constant of Gravitation
+Newton's Constant of Gravity
+Newton's Gravitational Constant
+Newton's Gravity Constant
+
+Constant of Gravitation
+Gravitational Constant
+
+avogadro's constant AKA
+avogadro's number
+
+Molar gas constant AKA
+ideal gas law constant
+ideal gas constant
+R
+
+Constants Here:
+electric constant
+magnetic constant
+newton gravitation
+planck constant
+planck length
+planck mass
+planck temperature
+planck time
+speed of light
+bohr magneton
+conductance quantum
+elementary charge
+josephson constant
+magnetic flux quantum
+nuclear magneton
+von klitzing
+atomic mass constant
+avogadro's constant
+boltzmann constant
+faraday constant
+first radiation constant
+loschmidt constant
+gas constant
+molar planck constant
+molar volume of ideal gas
+sackur-tetrode constant
+second radiation constant
+stefan boltzmann constant
+wien frequency displacement law constant 
+wien wavelength displacement law constant
+gravity
+alpha particle mass
+bohr radius
+classical electron radius
+compton wavelength
+deuteron mass
+electron mass
+fermi coupling constant
+fine structure constant
+hartree energy
+helion mass
+muon mass
+proton mass
+neutron mass
+rydberg constant
+tau mass
+triton mass
+
+
+What is the speed of light
+
---
+++ constants.js
@@ -0,0 +1,194 @@
+{
+    // Universal Constants
+    "electric constant": {
+        "value": "8.854187 * 10^-12 farads per meter",
+        "prefix": "The Electric Constant is "
+    },
+    "magnetic constant": {
+        "value": "12.566370 * 10^-7 newtons per ampere squared",
+        "prefix": "The Magnetic Constant is "
+    },
+    "newton gravitation": {
+        "value": "6.67408 * 10^-11 meters cubed per kilogram second squared",
+        "prefix": "The Newtonian Constant of Gravitation is "
+    },
+    "planck constant": {
+        "value": "6.626070 * 10-34 joule seconds",
+        "prefix": "The Planck Constant is "
+    },
+    "planck length": {
+      "value": "1.616229 * 10^-35 meters",
+      "prefix": "Planck Length is "
+    },
+    "planck mass": {
+      "value": "2.176470 * 10^-8 kilograms",
+      "prefix": "Planck Mass is "
+    },
+    "planck temperature": {
+        "value": "1.416808 * 10^32 kelvin",
+        "prefix": "Planck Temperature is "
+    },
+    "planck time": {
+        "value": "5.39116 * 10^-44 seconds",
+        "prefix": "Planck Time is "
+    },
+    "speed of light": {
+        "value": "299792458 meters per second",
+        "prefix": "The speed of light in a vacuum is "
+    },
+    // Electromagnetic Constants
+    "bohr magneton": {
+        "value": "927.400999 * 10^-26 joules per tesla",
+        "prefix": "The Bohr Magneton is "
+    },
+    "conductance quantum": {
+        "value": "7.748092 * 10^-5 siemens",
+        "prefix": "The conductance quantum is "
+    },
+    "elementary charge": {
+        "value": "1.602177 * 10^-19 coulombs",
+        "prefix": "The elementary charge constant is "
+    },
+    "josephson constant": {
+        "value": "483597.8525 * 10^9 hertz per volt",
+        "prefix": "The Josephson Constant is "
+    },
+    "magnetic flux quantum": {
+        "value": "2.067833 * 10^-15 webers",
+        "prefix": "The Magnetic Flux Quantum is "
+    },
+    "nuclear magneton": {
+        "value": "3.152451 * 10^-8 electronvolts per tesla",
+        "prefix": "The Nuclear Magneton is "
+    },
+    "von klitzing": {
+        "value": "25812.807455 ohms",
+        "prefix": "The von Klitzing constant is "
+    },
+    // Chemistry Constants
+    "atomic mass constant": {
+        "value": "1.660539 * 10^-27 kg",
+        "prefix": "The Atomic Mass Constant is "
+    },
+    "avogadro's constant": {
+        "value": "6.022140 * 10^23 inverse moles",
+        "prefix": "Avogadro's constant is "
+    },
+    "boltzmann constant": {
+        "value": "1.380648 * 10^-23 joules per kelvin",
+        "prefix": "Boltzmann's constant is "
+    },
+    "faraday constant": {
+        "value": "96485.33289 coulombs per mole",
+        "prefix": "The Faraday Constant is "
+    },
+    "first radiation constant": {
+        "value": "3.741771 watt meters squared",
+        "prefix": "The First Radiation Constant is "
+    },
+    "loschmidt constant": {
+        "value": "2.686781 * 10^25 inverse meters cubed",
+        "prefix": "The Loschmidt Constant at 0 degrees celsius and 1 atmosphere of pressure is "
+    },
+    "gas constant": {
+        "value": "8.3144598 joules per mole kelvin",
+        "prefix": "The Gas Constant, R, is "
+    },
+    "molar planck constant": {
+        "value": "3.990312 joule seconds per mole",
+        "prefix": "The Molar Planck Constant is "
+    },
+    "molar volume of ideal gas": {
+        "value": "222.413962 * 10^-3 meters cubed per mole",
+        "prefix": "The Molar Volume of Ideal Gas at 0 degrees celsius and 1 atmosphere of pressure is "
+    },
+    "sackur-tetrode constant": {
+        "value": "-1.1648714",
+        "prefix": "The Sackur-Tetrode constant at 0 degrees celsius and 1 atmosphere of pressure is "
+    },
+    "second radiation constant": {
+        "value": "1.438777 * 10^-2 meter kelvins",
+        "prefix": "The second radiation constant is "
+    },
+    "stefan boltzmann constant": {
+        "value": "5.670367 * 10^-8 watts per meter squared kelvin to the power of four",
+        "prefix": "The Stefan-Boltzmann constant is "
+    },
+    "wien frequency displacement law constant ": {
+        "value": "5.8789238 * 10^10 hertz per kelvin",
+        "prefix": "The Wien Frequency Displacement Law Constant is "
+    },
+    "wien wavelength displacement law constant": {
+        "value": "2.8977729 * 10^-3 meter kelvins",
+        "prefix": "The Wien Wavelength Displacement Law Constant is "
+    },
+    // Other
+    "gravity": {
+        "value": "9.81 meters per second squared",
+        "prefix": "Standard gravity is "
+    },
+    "alpha particle mass": {
+        "value": "6.644657 * 10^-27 kilograms",
+        "prefix": "Alpha Particle Mass is "
+    },
+    "bohr radius": {
+        "value": "0.529177 * 10^-10 meters",
+        "prefix": "The Bohr Radius is "
+    },
+    "classical electron radius": {
+        "value": "2.817940 * 10^-15 meters",
+        "prefix": "The Classical Electron Radius is "
+    },
+    "compton wavelength": {
+        "value": "2.426310 * 10^-12 meters",
+        "prefix": "The Compton Wavelength is "
+    },
+    "deuteron mass": {
+        "value": "3.343583 * 10^-27 kilograms",
+        "prefix": "Deuteron mass is "
+    },
+    "electron mass": {
+        "value": "9.109383 * 10^-31 kilograms",
+        "prefix": "Electron mass is "
+    },
+    "fermi coupling constant": {
+        "value": "1.1663787 * 10-5 inverse gigaelectronvolts squared",
+        "prefix": "The Fermi Coupling Constant is "
+    },
+    "fine structure constant": {
+        "value": "7.2973526 * 10^-3",
+        "prefix": "The Fine-structure constant is "
+    },
+    "hartree energy": {
+        "value": "4.359744 * 10^18 joules",
+        "prefix": "Hartree Energy is "
+    },
+    "helion mass": {
+        "value": "5.006412 * 10^-27 kilograms",
+        "prefix": "Helion Mass is "
+    },
+    "muon mass": {
+        "value": "1.883531 * 10^-28 kilograms",
+        "prefix": "Muon mass is "
+    },
+    "proton mass": {
+        "value": "1.672621 * 10^-27 kilograms",
+        "prefix": "Proton mass is "
+    },
+    "neutron mass": {
+        "value": "1.674927 * 10^-27 kilograms",
+        "prefix": "Neutron mass is "
+    },
+    "rydberg constant": {
+        "value": "10973731.568508 inverse meters",
+        "prefix": "The Rydberg Constant is "
+    },
+    "tau mass": {
+        "value": "3.16747 * 10^-27 kilograms",
+        "prefix": "Tau mass is "   
+    },
+    "triton mass": {
+        "value": "5.007357 * 10^-27 kilograms",
+        "prefix": "Trion mass is "
+    }
+}
